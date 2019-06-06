b1b9e22efa66a0f299f086d356e18bc0aabd36b4
Nuria Romero <anromercas@gmail.com>
2019-06-06 07:45:53 +0200

    arreglo package-lock

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,7 +1,7 @@
 // tslint:disable-next-line:eofline
- export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_SERVICIOS = 'http://localhost:3000';
 // export const URL_SERVICIOS = 'http://observaciones-preventivas.tk:3000';
 // export const URL_SERVICIOS = 'http://www.observacionespreventivas.tk:3000';
-// export const URL_SERVICIOS = 'http://46.101.99.189:3000';
+ export const URL_SERVICIOS = 'http://46.101.99.189:3000';
 //    export const URL_SERVICIOS = 'http://pruebas-ssl.tk:3000';
 // export const URL_SERVICIOS = 'http://207.154.253.122:8080';
